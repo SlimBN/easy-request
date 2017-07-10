24335361227d4cf43168a3c1d63c97d64cf6c4cf
Jesús Ángel González Novez <jesusgonzaleznovez@gmail.com>
2017-07-10 21:49:23 +0200

    added test

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
-/* jshint esversion: 6 */
 const request = require('request');
-const Q = require('q');
+const Q       = require('q');
 
-class EasyRequest{
-    
-    constructor(){
+module.exports = (url, parameters) => {
+    let promise = Q.defer();
+
+    if (!parameters instanceof Object || parameters === null || typeof parameters === 'undefined') {
+
+        promise.reject('Parameters type error');
+
+    } else {
+
+        request({
+            method: 'POST',
+            url   : url,
+            json  : true, // indicates the returning data is JSON, no need for JSON.parse()
+            body  : parameters
+        }, (err, res, body) => {
+            if (err) {
+                promise.reject(err);
+            } else {
+                promise.resolve(body);
+            }
+        });
 
     }
 
-    static post(url,parameters) {
-        let promise = Q.defer();
-
-        if(!parameters instanceof Object || parameters === null || typeof parameters === 'undefined'){
-
-            promise.reject('Parameters type error');
-
-        }else {
-
-            request({
-                method: 'POST',
-                url: url,
-                json: true, // indicates the returning data is JSON, no need for JSON.parse()
-                body:parameters
-            },  (err, res, body) => {
-                if(err){
-                    promise.reject(err);
-                } else {
-                    promise.resolve(body);
-                }
-            });
-            
-        }
-
-        return promise.promise;
-    }
-}
-module.exports = EasyRequest;
+    return promise.promise;
+};
 
